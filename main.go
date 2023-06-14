918808dbaf24f188544c80deff0ab04c207d8377
Luiz Fernando <nandoin@live.com>
2023-06-14 19:50:42 -0300

    change app

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("subiu rapaziada via argocd full pipeline"))
+		w.Write([]byte("argo automatic k8s deployment kustomized"))
 	})
 	http.ListenAndServe(":8090", nil)
 }
